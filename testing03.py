a2c3fe8b8deffc22196f72266b9541cf826d3a9d
Sercan Çavuş <sercancvs@protonmail.com>
2021-12-21 16:51:46 +0300

    testing03 string similarity

--- testing03.py
+++ testing03.py
@@ -1,21 +1,23 @@
-baseQuestions = {"1- Soru örneği 1": "Cevap örneği 1", "2- Soru örneği 2": "Cevap örneği 2", "3- Soru örneği 3":"Cevap örneği 3", "4- Soru örneği 4": "Cevap örneği 4"}
+from strsimpy.longest_common_subsequence import LongestCommonSubsequence
 
-# def test(y): hatalı, henüz alınan değeri bölüp liste içerisindeki keylerde aratamıyoruz.
-#     if y.split() in baseQuestions:
-#         print("başarılı")
+lcs = LongestCommonSubsequence()
+x = "Boğa burcunun rengi nedir?"
+x_x = "Rengi sarı"
+y = "Boğa burcunun günleri nedir?"
+y_y = "Boğa burcunun günü salı"
+b = "Boğa burcu para kazanır mı?"
+b_b = "Boğa burcu parayı iyi kazanır"
+c = input(": ")
+print("value")
+print(lcs.distance(str(x), c))
+print(lcs.distance(str(y), c))
+print(lcs.distance(str(b), c))
+if lcs.distance(str(x), c) <= 7:
+    print(x_x)
+elif lcs.distance(str(y), c) <= 7:
+    print(y_y)
+elif lcs.distance(str(b), c) <= 7:
+    print(b_b)
+else:
+    print("Söylediğinizi anlayamıyorum.")
 
-for word in baseQuestions.keys():
-    print(word)
-print("Hangi soruyu seçmek istersin? ")
-x = input("User: ")
-
-#test(x) hatalı, henüz alınan değeri bölüp liste içerisindeki keylerde aratamıyoruz.
-
-if x == "1":
-    print(baseQuestions.get("1- Soru örneği 1"))
-elif x == "2":
-    print(baseQuestions.get("1- Soru örneği 2"))
-elif x == "3":
-    print(baseQuestions.get("1- Soru örneği 3"))
-elif x == "4":
-    print(baseQuestions.get("1- Soru örneği 4"))
